9214210f8462f7c7bbfaaa0f26f018024c4dd86a
Derick Sheldrick <sheldrickderick9@gmail.com>
2025-04-17 19:50:20 +0300

    state practise

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+
+  const [isOn, setIsOn] = useState()
+
+  function handleClick(event){
+    setIsOn((isOn) => !isOn) // this is a setter function takes the variable isOn 
+
+  }
+
+  const color = isOn ? "red" : "white"; 
+
+  return <button onClick={handleClick} style={{background:color}}>{isOn ? "On" : "Off"}</button>;//instead of writing off I should use tenary so that the word can also rendered to reflect the condition
 }
 
 export default Toggle;
